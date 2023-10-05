cbb7c57adb8f23884a3f57a0ef17b3511b86dabe
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-10-05 14:03:08 +1100

    Debugging API url query. It works with direct injection of the api key in the query.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 //Unsplash Api
 const count = 10 ;
 const apiKey = '5hSAcrO4UMHUCHRRe21GmN1xwqM1Qnlq-Jjbsu_H_IE';
-const apiUrl = 'https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}';
+const apiUrl = 'https://api.unsplash.com/photos/random/?client_id=5hSAcrO4UMHUCHRRe21GmN1xwqM1Qnlq-Jjbsu_H_IE';
 
 // Get photos from UnSpash
-async function getPhotos(){
+async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
