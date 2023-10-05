36ce5fd9db340c001e37531b11811bc13e431ec2
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-10-05 13:17:47 +1100

    Create functions for getting photos from api and calling the function

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
 //Unsplash Api
 const count = 10 ;
-const apiKey = '';
+const apiKey = '5hSAcrO4UMHUCHRRe21GmN1xwqM1Qnlq-Jjbsu_H_IE';
 const apiUrl = 'https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}';
+
+// Get photos from UnSpash
+async function getPhotos(){
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);//Check if the Api url is loaded.
+    } catch (error) {
+        //Catch error here;
+    }
+}
+
+//Call getPhotos func
+getPhotos();
