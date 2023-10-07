0fccead6bd8bdc1fa9a66f17b1fa727e61af0d04
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2023-10-07 22:14:03 +1100

    Set global var to populate photo data

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+
+// Loading images
+const imageContainer = document.getElementById('imageContainer');
+const loader = document.getElementById('loader');
+
+//Set global variable to accomodate with the value changing everytime by requests.
+let photosArray = [];   
+
 //Unsplash Api
 const count = 10 ;
 const apiKey = '5hSAcrO4UMHUCHRRe21GmN1xwqM1Qnlq-Jjbsu_H_IE';
@@ -7,8 +15,8 @@ const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&coun
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);//Check if the Api url is loaded.
+        photosArray = await response.json(); //Before: const data = await response.json();
+        console.log(photosArray);//Before console.log(data);//Check if the Api url is populated.
     } catch (error) {
         //Catch error here;
     }
